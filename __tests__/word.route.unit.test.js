8c1457b39f10c51f5cad72b46a80c511f122a11f
sohammondal <soham.pf.mondal@gmail.com>
2020-02-29 01:03:14 +0530

    add unit test cases for getWordExamples function

--- __tests__/word.route.unit.test.js
+++ __tests__/word.route.unit.test.js
@@ -1,9 +1,22 @@
-const { getRandomWord } = require('../lib/routes');
+const { getRandomWord, getWordExamples } = require('../lib/routes');
+
+const mockKnownWord = 'shift';
+const mockUnknownWord = 'uplift';
 
 describe('Testing Word Routes', () => {
 
     it('should return a random string', () => {
         expect(getRandomWord()).resolves.toBeInstanceOf(String);
+    })
+
+    describe('/word/{word}/examples route test', () => {
+        it('should return examples of known word', () => {
+            expect(getWordExamples(mockKnownWord)).resolves.toBeInstanceOf(Array);
+        })
+
+        it('should throw error for examples of unknown word', () => {
+            expect(getWordExamples(mockUnknownWord)).rejects.toBe('Error: word not found');
+        })
     })
 
 })
