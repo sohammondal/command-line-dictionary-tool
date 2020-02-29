365f347ecf5011b2608f5a18c436a3eab231bee3
sohammondal <soham.pf.mondal@gmail.com>
2020-02-29 11:49:43 +0530

    add control-flow skeleton

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 #!/usr/bin/env node
+
+const main = (args = process.argv.slice(2)) => {
+
+    const option = args[0];
+    const word = args[1];
+
+    switch (option) {
+
+        case 'defn': // Word Definitions
+
+            break;
+
+        case 'syn': // Word Synonyms
+
+            break;
+
+        case 'ant': // Word Antonyms
+
+            break;
+
+        case 'ex': // Word Examples
+
+            break;
+
+        case 'play': // Word Game
+
+            break;
+
+        default:
+
+            if (option) { // Given Word Full Dict
+
+            } else { // Word of the Day - Random Word Full Dict
+
+            }
+
+            break;
+    }
+
+}
+
+
+main();
